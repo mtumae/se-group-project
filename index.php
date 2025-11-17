ba6aaa20e8f531ca3b14fc6b2f3dd09b3238f0b9
inthezonewithme <158035989+inthezonewithme@users.noreply.github.com>
2025-11-17 18:23:40 +0300

    Committing before merging

--- index.php
+++ index.php
@@ -12,7 +12,9 @@ $component = new Components();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>StrathMart - Home</title>
-    <link rel="stylesheet" href="index.css">
+    <style>
+        
+    </style>
 </head>
 <body>
 <?php
